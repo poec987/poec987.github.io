8e8a9afdcdfbf26e2450ec8bb9e3c7e1cba5efb3
poec987 <poedev08@outlook.com>
2023-05-27 21:47:46 -0500

    Cool Stuff
    Epic

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$windows = "PoeGDPS"; //zip
-$android = "PoeGDPS"; //apk
-$tools = "toolspage";
-$demonlist = "demonlist";
-$youtube = "youtubeprofile";
-$discord = "discordserver";
-$about = "There is epic cool stuff and cool community :D";
-?>
