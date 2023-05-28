fe07ed2fd63f13cefccd96a4d727c4d3e90c6c75
poec987 <poedev08@outlook.com>
2023-05-27 21:29:06 -0500

    funny errorrr

---
+++ 404.php
@@ -0,0 +1,20 @@
+<html>
+
+<head>
+    <title>Not Found</title>
+    <link rel="stylesheet" type="text/css" href="files/style.css">
+</head>
+
+<body style="background: linear-gradient(#6D00FC, #330074);">
+    <center>
+        <img src="files/logo.png" style="max-width: 40%; height: auto;">
+
+        <img src="files/notfound.png" style="max-width: 30%; height: auto;">
+        
+        <div class="text">
+            <span class="white">This page don't exist lmao.</span>
+        </div>
+    </center>
+</body>
+
+</html>
