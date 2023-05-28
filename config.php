5964ed0404df72b53092fdf461ea9e86c48b1578
poec987 <poedev08@outlook.com>
2023-05-27 21:11:00 -0500

    Testing Stuff
    A

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$windows = "PoeGDPS"; //zip
+$android = "PoeGDPS"; //apk
+$tools = "toolspage";
+$demonlist = "demonlist";
+$youtube = "youtubeprofile";
+$discord = "discordserver";
+$about = "There is epic cool stuff and cool community :D";
+?>
